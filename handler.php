3bdcedbb4dc88f1da564264c14b1edb0b11ce8ac
itqop <kalentev@gipro.com>
2023-01-11 12:19:57 +0300

    second6

--- handler.php
+++ handler.php
@@ -1,4 +1,16 @@
 <?
+function out($var, $var_name = '') {
+    echo '<pre style="outline: 1px dashed red;padding:5px;margin:10px;color:white;background:black;">';
+    if ( !empty($var_name)) {
+            echo '<h3>' + $var_name + '</h3>';
+    }
+    if (is_string($var)) {
+            $var = htmlspecialchars($var);
+    }
+    print_r($var);
+    echo '</pre>';
+}
+
 function call($method, $params){
     $queryUrl = 'http://'.$_REQUEST['DOMAIN'].'/rest/'.$method.'.json';
     $queryData = http_build_query(array_merge($params, array("auth" => $_REQUEST['AUTH_ID'])));
