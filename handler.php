faabb0f60f91c71b00bc0461fb9e4a3076cce2ad
itqop <kalentev@gipro.com>
2023-01-11 10:58:41 +0300

    second1

--- handler.php
+++ handler.php
@@ -1,4 +1,29 @@
 <?
+function call($method, $params){
+    $queryUrl = 'http://'.$_REQUEST['DOMAIN'].'/rest/'.$method.'.json';
+    $queryData = http_build_query(array_merge($params, array("auth" => $_REQUEST['AUTH_ID'])));
+    
+    
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_POST => 1,
+            CURLOPT_HEADER => 0,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => $queryUrl,
+            CURLOPT_POSTFIELDS => $queryData,
+    ));
+    
+    
+    $result = json_decode(curl_exec($curl), true);
+    
+    curl_close($curl);
+    out($result);
+    
+    };
+
+call('tasks.task.update', ['taskId' => 29, 'fields' => ['TITLE' => 'YRA']]);
+
 file_put_contents(
    __DIR__ . '/log/' . time() . '.txt',
    var_export($_REQUEST, true)
